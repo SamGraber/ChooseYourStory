5997737cd67f5e8609226b7c850c7529e2af3d8a
SamGraber <authorsamgraber@gmail.com>
2017-02-06 16:38:45 -0500

    Drop api test
    Now that we've verified that http authentication works, we don't need this.

--- source/app.tsx
+++ source/app.tsx
@@ -1,19 +1,13 @@
 import * as React from 'react';
 
 import { Login } from './login/login';
-import { get } from './api/http';
 
 export class App extends React.Component<any, any> {
-	testApi(): void {
-		get('api/test').then(data => console.log(data));
-	}
-	
 	render(): any {
 		return (
 			<div>
 				<h1>Hello World!</h1>
 				<Login />
-				<button onClick={this.testApi}>Test API</button>
 			</div>
 		);
 	}
