47df6c0d1e8b09afa2b3c143a6e06f97c28b2669
SamGraber <authorsamgraber@gmail.com>
2017-02-06 16:54:36 -0500

    Added a container class

--- source/app.tsx
+++ source/app.tsx
@@ -6,7 +6,7 @@ import { Login } from './login/login';
 export class App extends React.Component<any, any> {
 	render(): any {
 		return (
-			<div>
+			<div className="container">
 				<h1>Choose your story</h1>
 				<ul className="nav nav-tabs">
 					<li role="presentation"><Link to="/myfriends">My Friends</Link></li>
