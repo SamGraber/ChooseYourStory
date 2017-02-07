9fa5cee4424521604a153e7ef50adc0409df54d8
SamGraber <authorsamgraber@gmail.com>
2017-02-07 15:23:03 -0500

    Show a user profile on the right of the nav pane

--- source/app.tsx
+++ source/app.tsx
@@ -2,17 +2,49 @@ import * as React from 'react';
 import { Link } from 'react-router';
 
 import { Login } from './login/login';
+import { get } from './api/http';
+import { currentUser$, IUser } from './api/user';
 
-export class App extends React.Component<any, any> {
+export interface IAppState {
+	currentUser: IUser
+}
+
+export class App extends React.Component<any, IAppState> {
+	componentWillMount(): void {
+		currentUser$.subscribe(currentUser => this.setState({ currentUser }));
+	}
+
+	profile(addons: any): any {
+		if (this.state.currentUser) {
+			return (
+				<ul className="nav navbar-nav navbar-right">
+					<li><a href="#">{this.state.currentUser.name}</a></li>
+					<li><img className="thumbnail" src={this.state.currentUser.picture} /></li>
+					{addons}
+				</ul>
+			);
+		}
+		return (
+			<ul className="nav navbar-nav navbar-right">
+				{addons}
+			</ul>
+		);
+	}
+	
 	render(): any {
 		return (
 			<div className="container">
-				<h1>Choose your story</h1>
-				<ul className="nav nav-tabs">
-					<li role="presentation"><Link to="/myfriends">My Friends</Link></li>
-					<li role="presentation"><Login /></li>
-				</ul>
+				<nav className="navbar">
+					<div className="navbar-header">
+						<a className="navbar-brand">Choose your story</a>
+					</div>
+					<ul className="nav navbar-nav">
+						<li><Link to="/myfriends">My Friends</Link></li>
+					</ul>
+					{this.profile(<li><Login /></li>)}
+				</nav>
 				{this.props.children}
+				<button onClick={() => get('api/users').then(console.log)}>Test</button>
 			</div>
 		);
 	}
